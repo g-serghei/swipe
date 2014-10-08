9dd5422f743c216c05e87ad95b974d46d29e489c
Serghei <phoenix92@inbox.ru>
2014-10-08 15:23:30 +0300

    fixed - revert swipe on button click

--- demo/swipe-0.1.js
+++ demo/swipe-0.1.js
@@ -9,65 +9,65 @@ $.fn.swipe = function(options){
 		onStart: null,
 		onStop: null
     }
-	
+
     options = $.extend(true, defaults, options);
     options = defaults;
-	
-	
-	
+
+
+
   	var that = $(this);
 	var swipe_wrapper = that.parent();
 	var buttons_wrapper_width = Object.keys(options.actions).length * options.buttonWidth;
 	var button_height = that.outerHeight();
-	
-	
+
+
 	options.elementClick = function(event){
 		revertSwipe(event.currentTarget);
-		options.elemClick.apply(event.currentTarget, [event]);	
+		options.elemClick.apply(event.currentTarget, [event]);
 	};
-	
+
 	// Attach on click event
     that.on('click', options.elementClick);
-	
+
 	// Hide horizontal scroll bar for parent
 	that.parent().css('overflow-x', 'hidden');
-	
+
 	$(this).draggable({
         axis: "x",
 		drag: function(event, ui){
-			
+
 			// Call onDrag callback
 			options.onDrag && options.onDrag(event, ui);
-			
+
 		},
         start: function(event, ui) {
-			
+
 			// Revert all swiped elements
 			revertSwipe(event.target);
 
 			// Deattach on click event when start dragging
             that.off('click');
-			
+
 			// Call onStart callback
 			options.onStart && options.onStart(event, ui);
         },
         stop: function(event, ui) {
-			
+
 			// Attach on click event back (after 300ms to prevent clicking on mouse up)
             setTimeout(function(){ that.on('click', options.elementClick); }, 300);
-            
+
 			// Call onStop callback
 			options.onStop && options.onStop(event, ui);
         },
         containment: [swipe_wrapper.offset().left, 0, swipe_wrapper.offset().left + buttons_wrapper_width, 0]
     });
 
-	
+
 	var btns = getButtons();
 	$.each(that, function(k, elem){
 		btns.clone(true).prependTo($(elem));
 	});
-	
+
 	function getButtons(){
 		var buttons_wrapper = $('<div></div>');
 		$.each(options.actions, function(k, action){
@@ -76,6 +76,7 @@ $.fn.swipe = function(options){
 			btn.on('click', function(e){
 				e.stopPropagation();
 				action.callback.apply(this, [e]);
+				revertSwipe(e.target);
 			});
 			$.each(action.htmlOptions, function(option_name, option_value){
 				btn.attr(option_name, option_value);
@@ -86,7 +87,7 @@ $.fn.swipe = function(options){
 			});
 			buttons_wrapper.append(btn);
 		});
-		
+
 		buttons_wrapper.addClass('swipe-buttons').css({
 			'position': 'absolute',
 			'left': -buttons_wrapper_width + 'px',
@@ -95,8 +96,8 @@ $.fn.swipe = function(options){
 		});
 		return buttons_wrapper;
 	}
-	
-	
+
+
 	function revertSwipe(current_element){
 		$.each(that, function(k, elem){
 			if(elem != current_element){
